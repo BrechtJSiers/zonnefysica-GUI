cefc0b76813db85f64f178510f3f4b9574721424
Brecht Siers <post@brechtjesiers.nl>
2024-12-05 12:15:11 +0100

    nog een keer

---
+++ src/zonnefysica/GUI.py
@@ -0,0 +1,63 @@
+import sys
+
+import pyqtgraph as pg
+from PySide6 import QtWidgets
+from PySide6.QtCore import Slot
+
+# PyQtGraph global options
+pg.setConfigOption("background", "w")
+pg.setConfigOption("foreground", "k")
+
+
+class UserInterface(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
+        vbox = QtWidgets.QVBoxLayout(central_widget)
+
+        hbox1 = QtWidgets.QHBoxLayout()
+        hbox2 = QtWidgets.QHBoxLayout()
+        hbox3 = QtWidgets.QHBoxLayout()
+        hbox4 = QtWidgets.QHBoxLayout()
+        hbox5 = QtWidgets.QHBoxLayout()
+        hbox6 = QtWidgets.QHBoxLayout()
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        vbox.addLayout(hbox4)
+        vbox.addLayout(hbox5)
+        vbox.addLayout(hbox6)
+
+        self.choose_order = QtWidgets.QSpinBox()
+        self.select = QtWidgets.QPushButton("Select")
+        hbox1.addWidget(self.choose_order)
+        hbox1.addWidget(self.select)
+
+        self.saved = QtWidgets.QLabel("Saved")
+        hbox2.addWidget(self.saved)
+        # add widget with 'saved' text after saving
+
+        self.determine = QtWidgets.QPushButton("Determine rotationperiod")
+        hbox3.addWidget(self.determine)
+
+        self.graph_rot = pg.PlotWidget()
+        hbox4.addWidget(self.graph_rot)
+
+        self.value_1_line = QtWidgets.QPushButton("Calculated periods per line")
+        # give list with added points
+        hbox5.addWidget(self.value_1_line)
+
+        self.give_answer = QtWidgets.QLabel("Mean value")
+        hbox6.addWidget(self.give_answer)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    ui = UserInterface()
+    ui.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
